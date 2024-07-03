600568ec861a3fdd3afcf01aa6caedbb0442498b
Suranga Sandaruwan <mpsurangasw1111@gmail.com>
2024-07-03 10:19:21 +0530

    created spring-boot project

---
+++ src/main/java/com/suranga/spring_boot_tutorial_1/SpringBootTutorial1Application.java
@@ -0,0 +1,13 @@
+package com.suranga.spring_boot_tutorial_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootTutorial1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootTutorial1Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/suranga/spring_boot_tutorial_1/SpringBootTutorial1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suranga.spring_boot_tutorial_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootTutorial1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
